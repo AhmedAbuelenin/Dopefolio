9b4ccabbce875dfcada172b3aa32a911bff82aa0
Ahmed Abuelenin <ahprogsolutions@gmail.com>
2023-08-28 12:09:37 +0300

    added scrollMagic lib to add animations to content when scroll down

---
+++ js/scrollMagic.js
@@ -0,0 +1,31 @@
+var controller =
+  new ScrollMagic.Controller()
+
+function createScene(
+  triggerElement,
+  triggerHook = 0.8
+) {
+  return new ScrollMagic.Scene({
+    triggerElement: triggerElement,
+    triggerHook,
+    reverse: false
+  })
+    .setClassToggle(
+      triggerElement,
+      'reveal--visible'
+    )
+    .addTo(controller)
+}
+
+createScene('.heading-primary')
+createScene('.text-primary')
+createScene('.about__content-main')
+createScene('.about__content-skills')
+createScene('.projects__row')
+createScene('#contact-subheader')
+createScene('.contact__email-container')
+createScene('#social-heading')
+createScene('.main-footer__social-cont')
+createScene('#name-heading')
+createScene('.main-footer__short-desc')
+createScene('.main-footer__lower', 1)
